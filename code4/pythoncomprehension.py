3f8d04581da91065ff47cf9cac0d92ba8c6b198a
Ayush Shrivastava <ayush.shrivastavaujn@gmail.com>
2020-07-19 17:06:42 +0530

    python comprehension

---
+++ code4/pythoncomprehension.py
@@ -0,0 +1,12 @@
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+duplicates = []
+for value in some_list:
+    if some_list.count(value) > 1:
+        if value not in duplicates:
+            duplicates.append(value)
+
+print(duplicates)
+
+compdup = list({v for v in some_list if some_list.count(v)>1})
+print(compdup)
